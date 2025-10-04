480cf8968dc6579eb9d8130ea6610403e6c9252c
hurricanemod10 <hurricanemod10@gmail.com>
2025-10-04 16:58:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import argparse
 
-func main():
+def main():
     print("Effect Dir Editor v1.0 starting...")
     # This just ensures the build runs correctly.
     # Later we’ll connect this to read_effdir, write_effdir, and isolate_eff.
@@ -10,3 +10,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Effect Dir Editor v1.0")
     args = parser.parse_args()
     main()
+    
